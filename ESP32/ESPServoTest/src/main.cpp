399f23d41f633cd7f23dd71a3bfb52446980f227
Valker123 <vincentyl505@gmail.com>
2026-08-06 16:56:42 -0500

    added states for light switch

--- ESP32/ESPServoTest/src/main.cpp
+++ ESP32/ESPServoTest/src/main.cpp
@@ -5,50 +5,38 @@ Servo lightSwitchServo;
 
 const int servoSignal = 13;
 enum LightState {
-  LIGHT_SWITCH_ON
+  LIGHT_SWITCH_ON,
   LIGHT_SWITCH_OFF
-}
+};
 
 LightState currentLightState = LIGHT_SWITCH_OFF
 
-const int LightSwitchOnAngle = 500
-const int LightSwitchOffAngle = 2500
+const int LightSwitchOnAngle = 500;
+const int LightSwitchOffAngle = 2500;
 
 void setup() {
   ESP32PWM::allocateTimer(0);
   
-  lightSwitchServo.writeMicroseconds(500);
+  lightSwitchServo.writeMicroseconds(1500);
   lightSwitchServo.attach(servoSignal, 500, 2500);
   
   delay(1000);
 }
 
-void turnLightOn {
+void turnLightOn() {
   lightSwitchServo.writeMicroseconds(LightSwitchOnAngle);
   currentLightState = LIGHT_SWITCH_ON;
-  Serial.println("Light: ON")
+  Serial.println("Light: ON");
 }
 
-void turnLightOff {
+void turnLightOff() {
   lightSwitchServo.writeMicroseconds(LightSwitchOffAngle);
   currentLightState = LIGHT_SWITCH_OFF;
-  Serial.println("Light: OFF")
+  Serial.println("Light: OFF");
 }
 
 
 
 void loop() {
-  for (int pos = 500; pos <= 2500; pos += 20) {
-    lightSwitchServo.writeMicroseconds(pos);
-    delay(20);
-  }
-
-  delay(500);
-
-  for (int pos = 2500; pos >= 500; pos -= 20) {
-    lightSwitchServo.writeMicroseconds(pos);
-    delay(20);
-  }
-
-  delay(500);
+  
 }
